8c75e4c7832253277cebae0db413f50eda1f28d7
centaur3 <44935544+centaur3@users.noreply.github.com>
2025-07-18 22:41:58 -0500

    Update sign.js

--- sign.js
+++ sign.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const crypto = require('crypto');
+import express from 'express';
+import crypto from 'crypto';
 
 const app = express();
 app.use(express.json());
